d40de97846b61a5f2558c480257b4ffa09ba1770
SteveMustanski <steve.mustanski@gmail.com>
2019-07-12 12:22:36 -0400

    added draft cart functionality

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="content">
+      <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
@@ -35,6 +38,23 @@
         <button @click="selectNextBase()" class="next-selector">&#9660;</button>
       </div>
     </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -60,7 +80,8 @@ export default {
       selectedLeftArmIndex: 0,
       selectedRightArmIndex: 0,
       selectedTorsosIndex: 0,
-      selectedBaseIndex: 0
+      selectedBaseIndex: 0,
+      cart: []
     };
   },
   computed: {
@@ -134,6 +155,17 @@ export default {
         this.selectedBaseIndex,
         availableParts.bases.length
       );
+    },
+
+    addToCart() {
+      const robot = this.selectedRobot;
+      const cost =
+        robot.head.cost +
+        robot.leftArm.cost +
+        robot.rightArm.cost +
+        robot.torso.cost +
+        robot.base.cost;
+      this.cart.push(Object.assign({}, robot, { cost }));
     }
   }
 };
@@ -239,5 +271,28 @@ export default {
 
 .sale {
   color: red;
+}
+
+.content {
+  position: relative;
+}
+
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+
+td,
+th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+
+.cost {
+  text-align: right;
 }
 </style>
