d4973c58599a4fc0d0d1e4d033bdaa30c6f303a0
SteveMustanski <steve.mustanski@gmail.com>
2019-07-28 14:50:51 -0400

    added namespace for robots module

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -67,7 +67,7 @@ import CollapsibleSection from "../shared/CollapsibleSection.vue";
 export default {
   name: "RobotBuilder",
   created() {
-    this.$store.dispatch("getParts");
+    this.$store.dispatch("robots/getParts");
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -120,7 +120,7 @@ export default {
         robot.torso.cost +
         robot.base.cost;
       this.$store
-        .dispatch("addRobotToCart", Object.assign({}, robot, { cost }))
+        .dispatch("robots/addRobotToCart", Object.assign({}, robot, { cost }))
         .then(() => this.$router.push("/cart"));
       this.addedToCart = true;
     }
